25ccbd27d9c73bd088181ec73e0928d86d6d2187
Andrea Bottino <bottinoandrea93@gmail.com>
2024-05-07 23:28:09 +0100

    HistoryView: improved UI and implemented DetailView

--- SwiftLingo/Views/HistoryView.swift
+++ SwiftLingo/Views/HistoryView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct HistoryView: View {
     
     @Binding var translatorViewModel: TranslatorViewModel
+    @State private var selectedTranslation: TranslationModel?
     
     var body: some View {
-        List(translatorViewModel.history) {
-            Text($0.translation)
+        List {
+            ForEach(translatorViewModel.history) { translation in
+                Button {
+                    selectedTranslation = translation
+                } label: {
+                    SavedTranslationComponent(savedTranslation: translation)
+                }
+            }
+            .onDelete(perform: { indexSet in
+                print("deleted!")
+            })
+            .listRowSeparator(.hidden)
+            .listRowInsets(.init(top: 2.5, leading: 5, bottom: 2.5, trailing: 5))
+        }
+        .padding()
+        .listStyle(.plain)
+        .environment(\.defaultMinListRowHeight, 0)
+        .listRowSpacing(5)
+        .sheet(item: $selectedTranslation) { translation in
+            DetailView(savedTranslation: translation, deleteFromUserDefaults: {
+                print("Deleted!")
+            }, showingFirebaseTranslations: false)
         }
     }
 }
